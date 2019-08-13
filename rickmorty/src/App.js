a53912dc413fc3a3cb5bc52815200d82f79283fd
Victor Johnson <vjohnson22@gmail.com>
2019-08-13 17:11:33 -0400

    connected to api and displaying images on home. next create component for each episode

--- rickmorty/src/App.js
+++ rickmorty/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Link, Route} from 'react-router-dom'
+import Home from './Home/Home'
 
-function App() {
+class App extends React.Component{
+  
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <nav>
+          <Link to='/'>
+          < h1>Home</h1>
+          </Link>
+          <h1>Create</h1>
+          <h1>Update</h1>
+          <h1>Delete</h1>
+        </nav>
+        <main>
+          <Route component = {Home}/>
+        </main>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ rickmorty/src/Home/Home.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import axios from 'axios'
+
+class Home extends React.Component{
+    constructor(){
+        super()
+
+        this.state = {
+            data: []
+        }
+    }
+    componentDidMount() {
+        axios.get('https://mern-backend-vic.herokuapp.com')
+            
+            .then(res => {
+                this.setState({data:res.data})
+                console.log(res.data)
+            }) 
+    }
+    render(){
+        const episodes = this.state.data.map((episode, i) => {
+            return <img key = {i} src={episode.image.medium}/>
+            })
+        return(
+                <div>
+                    <h1>Episodes</h1>
+                    <ul>{episodes}</ul>
+                </div>  
+        )
+        
+    }
+}
+
+export default Home
